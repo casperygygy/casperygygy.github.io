24900d2cc679e1573513c2fa1a12680e5716192f
casperygygy <casperygygy@gmail.com>
2017-06-16 13:47:02 +0300

    add

---
+++ NewLook/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+    pagerCustom: '.bxslider',
+    nextSelector: '.bx-next',
+    prevSelector: '.bx-prev',
+    nextText: '',
+    prevText: '',
+  });
+});
