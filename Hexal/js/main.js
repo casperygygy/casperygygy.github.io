8bdb51fc20aae6e14b90e6839176cc1546cf3c44
casperygygy <casperygygy@gmail.com>
2018-11-27 09:48:46 +0200

    reload
    reload

--- Hexal/js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  $('.bxslider').bxSlider({
-    pagerCustom: '.bxslider__pager',
-    nextText: '',
-    prevText: '',
-  });
-});
--- NewLook/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $('.bxslider').bxSlider({
-    pagerCustom: '.bxslider',
-    nextSelector: '.bx-next',
-    prevSelector: '.bx-prev',
-    nextText: '',
-    prevText: '',
-  });
-});
